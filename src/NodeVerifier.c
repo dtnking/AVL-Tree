f39f68de4e15fe14cbce6718991aab0777f90fe9
Danny Aw <kokwah07@hotmail.com>
2017-10-23 17:23:42 +0800

    Implemented a simplified TEST_ASSERT to test out the lenthy Testing program.

---
+++ src/NodeVerifier.c
@@ -0,0 +1,40 @@
+#include "NodeVerifier.h"
+#include "Node.h"
+#include "unity.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <malloc.h>
+
+
+char *createMessage(char *message, ...){
+  va_list args;
+  char *buffer;
+  int length;
+
+  va_start(args,message);
+
+  length = vsnprintf(buffer, 0, message, args);
+  buffer = malloc(length+1);
+  vsnprintf(buffer, length+1, message, args);
+
+  return buffer;
+}
+
+void testAssertEqualNode(Node *left, Node *right, int bf, Node *node,int lineNo){
+  char *error;
+  if(left != NULL){
+    if(node->left == NULL){
+      error = createMessage("Expected left node to be 0x%p, but was %p",  \
+                            left,node->left);
+    UNITY_TEST_FAIL(lineNo,error);
+    }
+  }
+
+  if(right != NULL){
+    if(node->right == NULL){
+      error = createMessage("Expected right node to be 0x%p, but was %p",  \
+                            right,node->right);
+    UNITY_TEST_FAIL(lineNo,error);
+    }
+  }
+}
