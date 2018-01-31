b169b97df745ad1adcc3b92b0e3ff22d37e54c2a
Danny Aw <kokwah07@hotmail.com>
2018-01-31 13:16:10 +0800

    Typecasted pointer to int to compare

--- src/AvlInteger.c
+++ src/AvlInteger.c
@@ -1,10 +1,10 @@
 #include "AvlInteger.h"
 
 int IntegerCompare(int nodeData,intNode *refNode){
-
-  if(nodeData > refNode->data)
+  int refData = (int)refNode->data;
+  if(nodeData > refData)
     return 1;
-  else if(nodeData < refNode->data)
+  else if(nodeData < refData)
     return -1;
   else
     return 0;
